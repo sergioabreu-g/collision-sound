5dd588777ae606e8b5e539d5aff662b357383773
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-12-03 13:17:46 +0100

    Fmod for Unity

---
+++ physical_sound/Assets/Scripts/PhysicalSound.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PhysicalSound
+{
+    static class Manager
+    {
+        static private List<Tuple<SoundCollider, SoundCollider>> _collisions;
+        
+        public static void collisionDetected(SoundCollider yourself, SoundCollider other)
+        {
+            if (_collisions == null) _collisions = new List<Tuple<SoundCollider, SoundCollider>>();
+
+            Tuple<SoundCollider, SoundCollider> collision = new Tuple<SoundCollider, SoundCollider>(yourself, other);
+
+            foreach (Tuple<SoundCollider, SoundCollider> c in _collisions) {
+                if (c.Item1 == collision.Item2 && c.Item2 == collision.Item1)
+                {
+                    _collisions.Remove(c);
+                    return;
+                }
+            }
+
+            _collisions.Add(collision);
+            playCollisionSound();
+        }
+
+        private static void playCollisionSound()
+        {
+
+        }
+    }
+}
