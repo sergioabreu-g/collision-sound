021a7b7ee7cddc7b28350876bf48b22070f0b264
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-12-04 23:57:33 +0100

    Manager core functionality (dirty)

--- physical_sound/Assets/Scripts/PhysicalSound.cs
+++ physical_sound/Assets/Scripts/PhysicalSound.cs
@@ -9,17 +9,50 @@ namespace PhysicalSound
 {
     static class Manager
     {
+        // 2D Matrix of event descriptions for the collisions between materials
+        static private Dictionary<string, Dictionary<string, FMOD.Studio.EventDescription>> _soundEvents;
         static private List<Tuple<SoundCollider, SoundCollider>> _collisions;
         
         static Manager() {
-            FMOD.Studio.Bank c;
-            FMODUnity.RuntimeManager.StudioSystem.getBank("bank:/Master Bank", out c);
+            FMOD.Studio.Bank bank;
+            FMODUnity.RuntimeManager.StudioSystem.getBank("bank:/Master Bank", out bank);
 
-            FMOD.Studio.EventDescription[] descriptions;
-            c.getEventList(out descriptions);
-            foreach (FMOD.Studio.EventDescription des in descriptions)
+            FMOD.Studio.EventDescription[] eventDescriptions;
+            bank.getEventList(out eventDescriptions);
+
+            _soundEvents = new Dictionary<string, Dictionary<string, FMOD.Studio.EventDescription>>();
+
+            foreach (FMOD.Studio.EventDescription description in eventDescriptions)
             {
-                // LOAD MATERIAL INTERACTIONS IN AN ARRAY
+                string path;
+                description.getPath(out path);
+                path = path.Split(':')[1];
+
+                string[] temp = path.Split('/');
+                string material = temp[1];
+                string collidesWith = temp[2];
+
+                // Adds a new material it it hasn't already been added
+                Dictionary<string, FMOD.Studio.EventDescription> materialEvents;
+                if (!_soundEvents.TryGetValue(material, out materialEvents)) {
+                    materialEvents = new Dictionary<string, FMOD.Studio.EventDescription>();
+                    _soundEvents.Add(material, materialEvents);
+                }
+                if (!materialEvents.ContainsKey(collidesWith)) materialEvents.Add(collidesWith, description);
+
+                if (material == collidesWith) continue;
+
+
+                // Adds the other material so the same event description can be retrieved
+                // no matter the order given
+                // [material1][material2] = [material2][material1]
+
+                Dictionary<string, FMOD.Studio.EventDescription> collidesWithEvents;
+                if (!_soundEvents.TryGetValue(collidesWith, out collidesWithEvents)) {
+                    collidesWithEvents = new Dictionary<string, FMOD.Studio.EventDescription>();
+                    _soundEvents.Add(collidesWith, collidesWithEvents);
+                }
+                if (!collidesWithEvents.ContainsKey(material)) collidesWithEvents.Add(material, description);
             }
         }
 
@@ -48,7 +81,33 @@ namespace PhysicalSound
 
         private static void playCollisionSound(SoundCollider yourself, SoundCollider other)
         {
-            yourself.getEventEmitter().Play();
+            if (!_soundEvents.ContainsKey(yourself.soundMaterial) || !_soundEvents.ContainsKey(other.soundMaterial)) {
+#if UNITY_EDITOR
+                Debug.LogError("Sound material name not found");
+#endif
+                return;
+            }
+
+            // If there is a particular event defined for this collision, play it
+            FMOD.Studio.EventDescription desc;
+            if (_soundEvents[yourself.soundMaterial].TryGetValue(other.soundMaterial, out desc)) {
+                string path;
+                desc.getPath(out path);
+                yourself.getEventEmitter().Event = path;
+                yourself.getEventEmitter().Play();
+            }
+
+            else { // Else, play each sound material default sound
+                string path;
+                _soundEvents[yourself.soundMaterial][yourself.soundMaterial].getPath(out path);
+                yourself.getEventEmitter().Event = path;
+
+                _soundEvents[other.soundMaterial][other.soundMaterial].getPath(out path);
+                other.getEventEmitter().Event = path;
+
+                yourself.getEventEmitter().Play();
+                other.getEventEmitter().Play();
+            }
         }
 
         private static void playCollisionSound(SoundCollider yourself)
--- physical_sound/Assets/Scripts/SoundCollider.cs
+++ physical_sound/Assets/Scripts/SoundCollider.cs
@@ -7,8 +7,7 @@ namespace PhysicalSound
     [RequireComponent(typeof(FMODUnity.StudioEventEmitter))]
     public class SoundCollider : MonoBehaviour
     {
-        [FMODUnity.EventRef]
-        public string soundMaterial = "Wood";
+        public string soundMaterial = "wood";
 
         [Tooltip("If set to true, sounds will only be played if the" +
             "other gameobject also has a SoundMaterial component.")]
@@ -33,54 +32,34 @@ namespace PhysicalSound
         // Collision detection for every possible scenario
         private void OnCollisionEnter(Collision collision)
         {
-            if (!requireAnotherSoundMaterial)
-            {
-                genericCollisionSound();
-                return;
-            }
-
             SoundCollider collidedWith = collision.gameObject.GetComponent<SoundCollider>();
             if (collidedWith != null)
                 collisionSound(collidedWith);
+            else if (!requireAnotherSoundMaterial) genericCollisionSound();
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!requireAnotherSoundMaterial)
-            {
-                genericCollisionSound();
-                return;
-            }
-
             SoundCollider collidedWith = other.gameObject.GetComponent<SoundCollider>();
             if (collidedWith != null)
                 collisionSound(collidedWith);
+            else if (!requireAnotherSoundMaterial) genericCollisionSound();
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (!requireAnotherSoundMaterial)
-            {
-                genericCollisionSound();
-                return;
-            }
-
             SoundCollider collidedWith = collision.gameObject.GetComponent<SoundCollider>();
             if (collidedWith != null)
                 collisionSound(collidedWith);
+            else if (!requireAnotherSoundMaterial) genericCollisionSound();
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (!requireAnotherSoundMaterial)
-            {
-                genericCollisionSound();
-                return;
-            }
-
             SoundCollider collidedWith = collision.gameObject.GetComponent<SoundCollider>();
             if (collidedWith != null)
                 collisionSound(collidedWith);
+            else if (!requireAnotherSoundMaterial) genericCollisionSound();
         }
 
         public FMODUnity.StudioEventEmitter getEventEmitter()
