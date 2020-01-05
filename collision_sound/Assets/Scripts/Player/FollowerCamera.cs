b08ef5373e1ba0453dae6d4dcce54587df8bda7f
Sergio Abreu García <saggamedeveloper@gmail.com>
2020-01-05 19:52:41 +0100

    New demo started

---
+++ collision_sound/Assets/Scripts/Player/FollowerCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates the player body and its camera based on mouse input
+/// </summary>
+public class FollowerCamera : MonoBehaviour
+{
+    public GameObject whoToFollow;
+    public Vector3 offset;
+
+    public float sensivity = 100;
+    public float verticalClampAngle = 55;
+    public float horizontalClampAngle = 60;
+
+    private Vector3 _cameraRotation;
+
+    void Update()
+    {
+        Vector3 auxOffset = whoToFollow.transform.forward;
+        auxOffset *= offset.z;
+        auxOffset.x += offset.x;
+        auxOffset.y += offset.y;
+        transform.position = whoToFollow.transform.position + auxOffset;
+        transform.LookAt(whoToFollow.transform);
+
+        _cameraRotation.x += -Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+        _cameraRotation.y += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+
+        _cameraRotation.x = Mathf.Clamp(_cameraRotation.x, -verticalClampAngle, verticalClampAngle);
+        _cameraRotation.y = Mathf.Clamp(_cameraRotation.y, -horizontalClampAngle, horizontalClampAngle);
+
+        transform.localEulerAngles += _cameraRotation;
+    }
+}
---
+++ collision_sound/Assets/Scripts/Player/VehicleMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class VehicleMovement : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider) {
+        if (collider.transform.childCount == 0) {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate() {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
